7d172ada64880d6a8ba61ff0b1af1a2cbdb8f81b
Emmanuel <emma.balta09@gmail.com>
2026-04-23 21:28:59 -0600

    Add UserAccess model and ViewModel
    Introduce UserAccessModel to SharedResources.Models to represent user access records (IdUserAccess, IdUser, nullable User, ExpirationDate, Type). Add a scaffolded UserAccessViewModel in Acceso_UMAD_QRs.ViewModels inheriting CommunityToolkit.Mvvm.ComponentModel.ObservableObject to serve as the ViewModel for user access functionality.

---
+++ Acceso_UMAD_QRs/Acceso_UMAD_QRs/ViewModels/UserAccessViewModel.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Acceso_UMAD_QRs.ViewModels
+{
+    public partial class UserAccessViewModel : ObservableObject
+    {
+    }
+}
---
+++ Acceso_UMAD_QRs/SharedResources/Models/UserAccessModel.cs
@@ -0,0 +1,15 @@
+﻿namespace SharedResources.Models
+{
+    public class UserAccessModel
+    {
+        public int IdUserAccess { get; set; }
+
+        public int IdUser { get; set; }
+
+        public UserModel? User { get; set; }
+
+        public DateTime ExpirationDate { get; set; }
+
+        public bool Type { get; set; }
+    }
+}
